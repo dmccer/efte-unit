04f8914db152d24af3abecde3746c13ad3dd1a91
yunhua.xiao <yunhua.xiao@dianping.com>
2014-10-24 21:36:24 +0800

    cp

--- cp.js
+++ cp.js
@@ -1,4 +1,5 @@
 var spawn = require('child_process');
 var path = require('path');
 
-spawn('cp', ['-f', '']);
+console.log(NODE_PATH);
+spawn('cp', ['-f', path.join(__dirname, 'template', '*'), './']);
